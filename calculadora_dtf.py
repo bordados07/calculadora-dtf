1fa5690540c9c63acb322d6de92b247328898d53
bordados07 <gdlbordados06@gmail.com>
2026-09-23 17:19:41 -0600

    Change greeting message in DTF calculator

--- calculadora_dtf.py
+++ calculadora_dtf.py
@@ -10,7 +10,7 @@ ANCHO_ROLLO = 58.0
 LARGO_METRO = 100.0
 
 st.title("📦 Calculadora DTF PRO MAX 4K HD")
-st.write("HELLO WORD")
+st.write("12345")
 
 archivo = st.file_uploader("Sube una imagen", type=["png", "jpg", "jpeg"])
 
